f38179ec712787d871cb634fff45acb6934db8a3
maxxeg <maxxeg@gmail.com>
2020-05-15 18:27:40 +0400

    Ex17: Рефакторинг

--- src/test/java/tests/HomeWork.java
+++ src/test/java/tests/HomeWork.java
@@ -1,10 +1,7 @@
 package tests;
 import lib.CoreTestCase;
 import lib.Platform;
-import lib.ui.ArticlePageObject;
-import lib.ui.MyListsPageObject;
-import lib.ui.NavigationUI;
-import lib.ui.SearchPageObject;
+import lib.ui.*;
 import lib.ui.factories.ArticlePageObjectFactory;
 import lib.ui.factories.MyListsPageObjectFactory;
 import lib.ui.factories.NavigationUIFactory;
@@ -20,6 +17,9 @@ public class HomeWork extends CoreTestCase
   private ArticlePageObject ArticlePageObject;
   private NavigationUI NavigationUI;
   private MyListsPageObject MyListsPageObject;
+  private static final String
+          login = "mmaxxeg",
+          password = "epyfq2000";
 
 
   @Test
@@ -46,16 +46,16 @@ public class HomeWork extends CoreTestCase
   }
 
   @Test
-  public void testSave2ArticlesToList_Ex5_Ex11() throws InterruptedException
+  public void testSave2ArticlesToList_Ex5_Ex11_Ex17() throws InterruptedException
   {
     SearchPageObject = SearchPageObjectFactory.get(driver);
     ArticlePageObject = ArticlePageObjectFactory.get(driver);
     NavigationUI = NavigationUIFactory.get(driver);
     MyListsPageObject = MyListsPageObjectFactory.get(driver);
+    AuthorizationPageObject Auth = new AuthorizationPageObject(driver);
 
     String search_word = "Languages of";
     String name_of_folder = "My list";
-
 
     // добавляем первую статью
     SearchPageObject.initSearchInput(); // кликаем на строку поиска
@@ -68,64 +68,144 @@ public class HomeWork extends CoreTestCase
             amount_articles_title > 1
     );
 
-    List<WebElement> articles_title = SearchPageObject.allArticlesTitleOnSearchPage(); // получаем список заголовков статей
-    String article_title_from_search_1 = articles_title.get(0).getText(); // название первой статьи
-    String article_title_from_search_2 = articles_title.get(1).getText(); // название второй статьи
-    SearchPageObject.clickByArticleWithSubstring(article_title_from_search_1); // кликаем на заголовок 1-й статьи
-
+    // Android
     if (Platform.getInstance().isAndroid()) {
+      List<WebElement> articles_title = SearchPageObject.allArticlesTitleOnSearchPage(); // получаем список заголовков статей
+      String article_title_from_search_1 = articles_title.get(0).getText(); // название первой статьи
+      String article_title_from_search_2 = articles_title.get(1).getText(); // название второй статьи
+      SearchPageObject.clickByArticleWithSubstring(article_title_from_search_1); // кликаем на заголовок 1-й статьи
       ArticlePageObject.addArticleToMyList(name_of_folder); // создаем папку списка и добавляем в нее первую статью
-    } else {
-      ArticlePageObject.addArticlesToMySaved();
-      ArticlePageObject.clickOnEmptyPlaceToClosePopup(50, 50);
-    }
-
-    ArticlePageObject.closeArticle(); // возврат на Главную
-    SearchPageObject.initSearchInput(); // кликаем на строку поиска
-
-    if (Platform.getInstance().isAndroid()) {
+      ArticlePageObject.closeArticle(); // возврат на Главную
+      SearchPageObject.initSearchInput(); // кликаем на строку поиска
       SearchPageObject.typeSearchLine(search_word); // вводим поисковый запрос
-    }
-
-    SearchPageObject.clickByArticleWithSubstring(article_title_from_search_2); // кликаем на заголовок 2-й статьи
-
-    if (Platform.getInstance().isAndroid()) {
+      SearchPageObject.clickByArticleWithTitle(article_title_from_search_2); // кликаем на заголовок 2-й статьи
       ArticlePageObject.addArticle2ToMyList(name_of_folder); // добавляем в папку вторую статью
-    } else {
-      ArticlePageObject.addArticlesToMySaved();
-    }
-
-    ArticlePageObject.closeArticle(); // возврат на Главную
-    NavigationUI.clickMyLists(); // переходим к спискам статей
-
-    if (Platform.getInstance().isAndroid()) {
+      ArticlePageObject.closeArticle(); // возврат на Главную
+      NavigationUI.clickMyLists(); // переходим к спискам статей
       MyListsPageObject.openFolderByName(name_of_folder); // переходим в папку
-    }
 
-    int amount_articles_title_in_list = MyListsPageObject.amountOfAllArticlesTitleOnList(); // получаем количество заголовков
+      int amount_articles_title_in_list = MyListsPageObject.amountOfAllArticlesTitleOnList(); // получаем количество заголовков
 
-    // убеждаемся что добавлено 2-е статьи
-    Assert.assertTrue(
-            "Amount of articles in list <> 2",
-            amount_articles_title_in_list == 2
-    );
+      // убеждаемся что добавлено 2-е статьи
+      Assert.assertTrue(
+              "Amount of articles in list <> 2",
+              amount_articles_title_in_list == 2
+      );
 
-    MyListsPageObject.swipeByArticleToDelete(article_title_from_search_1); // делаем свайп для первой статьи и удаляем ее
+      MyListsPageObject.swipeByArticleToDelete(article_title_from_search_1); // делаем свайп для первой статьи и удаляем ее
 
-    int amount_articles_title_in_list_after_delete = MyListsPageObject.amountOfAllArticlesTitleOnList(); // получаем количество заголовков
+      int amount_articles_title_in_list_after_delete = MyListsPageObject.amountOfAllArticlesTitleOnList(); // получаем количество заголовков
 
-    // проверяем что осталась только одна статья в списке
-    Assert.
-            assertTrue(
-            "Amount of articles in list <> 1",
-            amount_articles_title_in_list_after_delete == 1
-    );
+      // проверяем что осталась только одна статья в списке
+      Assert.
+              assertTrue(
+                      "Amount of articles in list <> 1",
+                      amount_articles_title_in_list_after_delete == 1
+              );
 
-    MyListsPageObject.waitForArticleByTitleAndClick(article_title_from_search_2); // переходим на статью которая осталась
+      MyListsPageObject.waitForArticleByTitleAndClick(article_title_from_search_2); // переходим на статью которая осталась
 
-    if (Platform.getInstance().isAndroid()) {
       WebElement title_element = ArticlePageObject.waitForTitleElement(); // получаем элемент с названием на экране статьи
       String article_title = title_element.getAttribute("text"); // получаем название статьи
+
+      // сравниваем название статьи 2 из поиска и на экране статьи
+      Assert.assertEquals(
+                "We see unexpected title",
+                article_title_from_search_2,
+                article_title
+      );
+    }
+    // iOS
+    else if (Platform.getInstance().isIOS()) {
+      List<WebElement> articles_title = SearchPageObject.allArticlesTitleOnSearchPage(); // получаем список заголовков статей
+      String article_title_from_search_1 = articles_title.get(0).getText(); // название первой статьи
+      String article_title_from_search_2 = articles_title.get(1).getText(); // название второй статьи
+      SearchPageObject.clickByArticleWithSubstring(article_title_from_search_1); // кликаем на заголовок 1-й статьи
+      ArticlePageObject.addArticlesToMySaved();
+      ArticlePageObject.clickOnEmptyPlaceToClosePopup(50, 50);
+      ArticlePageObject.closeArticle(); // возврат на Главную
+      SearchPageObject.initSearchInput(); // кликаем на строку поиска
+      SearchPageObject.clickByArticleWithSubstring(article_title_from_search_2); // кликаем на заголовок 2-й статьи
+      ArticlePageObject.addArticlesToMySaved();
+      ArticlePageObject.closeArticle(); // возврат на Главную
+      NavigationUI.clickMyLists(); // переходим к спискам статей
+
+      int amount_articles_title_in_list = MyListsPageObject.amountOfAllArticlesTitleOnList(); // получаем количество заголовков
+
+      // убеждаемся что добавлено 2-е статьи
+      Assert.assertTrue(
+              "Amount of articles in list <> 2",
+              amount_articles_title_in_list == 2
+      );
+
+      MyListsPageObject.swipeByArticleToDelete(article_title_from_search_1); // делаем свайп для первой статьи и удаляем ее
+
+      int amount_articles_title_in_list_after_delete = MyListsPageObject.amountOfAllArticlesTitleOnList(); // получаем количество заголовков
+
+      // проверяем что осталась только одна статья в списке
+      Assert.
+              assertTrue(
+                      "Amount of articles in list <> 1",
+                      amount_articles_title_in_list_after_delete == 1
+              );
+
+      MyListsPageObject.waitForArticleByTitleAndClick(article_title_from_search_2); // переходим на статью которая осталась
+
+      ArticlePageObject.waitForElementPresent(
+                "id:" + article_title_from_search_2,
+                "Article title on page is not equal by '" + article_title_from_search_2 + "'");
+    }
+    // Mobile WEB
+    else if (Platform.getInstance().isMW()) {
+      List<WebElement> articles_title = SearchPageObject.allArticlesTitleOnSearchPage(); // получаем список заголовков статей
+      String article_title_from_search_1 = articles_title.get(0).getAttribute("title"); // название первой статьи
+      String article_title_from_search_2 = articles_title.get(1).getAttribute("title"); // название второй статьи
+      SearchPageObject.clickByArticleWithTitle(article_title_from_search_1); // кликаем на заголовок 1-й статьи
+      ArticlePageObject.addArticlesToMySaved(); // кликаем на иконку добавления статьи в список
+      // авторизуемся
+      Auth.clickAuthButton();
+      Auth.enterLoginData(login, password);
+      Auth.submitForm();
+      ArticlePageObject.waitForTitleElement(); // ждем появления названия статьи на странице после авторизации
+      assertEquals("We are not on the same page after login", article_title_from_search_1, ArticlePageObject.getArticleTitle()); // убеждаемся что это нужная статья
+      ArticlePageObject.addArticlesToMySaved(); // повторно кликаем на иконку добавления статьи в список
+
+      ///////// добавление второй статьи
+      SearchPageObject.initSearchInput(); // кликаем на строку поиска
+      SearchPageObject.typeSearchLine(search_word); // вводим поисковый запрос
+      SearchPageObject.clickByArticleWithTitle(article_title_from_search_2); // кликаем на заголовок 2-й статьи
+      ArticlePageObject.addArticlesToMySaved(); // кликаем на иконку добавления статьи в список
+
+      ///////// список сохраненных статей
+      NavigationUI.openNavigation(); // кликаем на бургер меню
+      NavigationUI.clickMyLists(); // кликаем на пункт перехода в список
+
+      int amount_articles_title_in_list = MyListsPageObject.amountOfAllArticlesTitleOnList(); // получаем количество заголовков
+
+      // убеждаемся что добавлено 2-е статьи
+      Assert.assertTrue(
+              "Amount of articles in list <> 2",
+              amount_articles_title_in_list == 2
+      );
+
+      MyListsPageObject.swipeByArticleToDelete(article_title_from_search_1); // удаляем первую статью и делаем обновление страницы
+
+      int amount_articles_title_in_list_after_delete = MyListsPageObject.amountOfAllArticlesTitleOnList(); // получаем количество заголовков
+
+      // проверяем что осталась только одна статья в списке
+      Assert.
+              assertTrue(
+                      "Amount of articles in list <> 1",
+                      amount_articles_title_in_list_after_delete == 1
+              );
+
+      MyListsPageObject.waitForArticleByTitleAndClick(article_title_from_search_2); // переходим на статью которая осталась
+
+      WebElement title_element = ArticlePageObject.waitForTitleElement(); // получаем элемент с названием на экране статьи
+      String article_title = title_element.getAttribute("text"); // получаем название статьи
+
+      System.out.println(title_element);
+      System.out.println(article_title);
 
       // сравниваем название статьи 2 из поиска и на экране статьи
       Assert.assertEquals(
@@ -133,10 +213,6 @@ public class HomeWork extends CoreTestCase
               article_title_from_search_2,
               article_title
       );
-    } else {
-      ArticlePageObject.waitForElementPresent(
-              "id:" + article_title_from_search_2,
-              "Article title on page is not equal by '" + article_title_from_search_2 + "'");
     }
   }
 
